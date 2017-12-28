9e346f6049e059e8f9ba7cfb8d9f4dc5c4c2dd7e
RAN <wangshuomailbox@163.com>
2017-12-28 19:25:15 +0800

    ws

--- src/pages/artist/index/index.js
+++ src/pages/artist/index/index.js
@@ -8,7 +8,44 @@ Page({
    */
   data: {
     title: 'Demo page',
-    styleSeries: ['现实分割', '差异风格', '工作风格', '现实分割',  '差异风格', '工作风格', '现实分割', '差异风格', '工作风格']
+    styleSeries: ['现实分割', '差异风格', '工作风格', '现实分割',  '差异风格', '工作风格', '现实分割', '差异风格', '工作风格'],
+    artictList:[
+      {
+        userId: '111', name: '今敏', avatar: 'http://image.artful.com.cn/artful/other/2017-12-22/a6509c8747e0dfb5!960xAuto',
+        list:[
+          {url: 'http://image.artful.com.cn/artful/other/2017-12-25/1a87be94254d4cd8!960xAuto', id: app.util.uuid()},
+          {url:  'http://image.artful.com.cn/artful/other/2017-12-22/a6509c8747e0dfb5!960xAuto', id: app.util.uuid()},
+          {url:  'http://image.artful.com.cn/artful/other/2017-12-22/30726fac9c4060d1!960xAuto', id: app.util.uuid()}]
+      },
+      {
+        userId: '111', name: '今敏', avatar: 'http://image.artful.com.cn/artful/other/2017-12-22/a6509c8747e0dfb5!960xAuto',
+        list:[
+          {url: 'http://image.artful.com.cn/artful/other/2017-12-25/1a87be94254d4cd8!960xAuto', id: app.util.uuid()},
+          {url:  'http://image.artful.com.cn/artful/other/2017-12-22/a6509c8747e0dfb5!960xAuto', id: app.util.uuid()},
+          {url:  'http://image.artful.com.cn/artful/other/2017-12-22/30726fac9c4060d1!960xAuto', id: app.util.uuid()}]
+      },
+      {
+        userId: '111', name: '今敏', avatar: 'http://image.artful.com.cn/artful/other/2017-12-22/a6509c8747e0dfb5!960xAuto',
+        list:[
+          {url: 'http://image.artful.com.cn/artful/other/2017-12-25/1a87be94254d4cd8!960xAuto', id: app.util.uuid()},
+          {url:  'http://image.artful.com.cn/artful/other/2017-12-22/a6509c8747e0dfb5!960xAuto', id: app.util.uuid()},
+          {url:  'http://image.artful.com.cn/artful/other/2017-12-22/30726fac9c4060d1!960xAuto', id: app.util.uuid()}]
+      },
+      {
+        userId: '111', name: '今敏', avatar: 'http://image.artful.com.cn/artful/other/2017-12-22/a6509c8747e0dfb5!960xAuto',
+        list:[
+          {url: 'http://image.artful.com.cn/artful/other/2017-12-25/1a87be94254d4cd8!960xAuto', id: app.util.uuid()},
+          {url:  'http://image.artful.com.cn/artful/other/2017-12-22/a6509c8747e0dfb5!960xAuto', id: app.util.uuid()},
+          {url:  'http://image.artful.com.cn/artful/other/2017-12-22/30726fac9c4060d1!960xAuto', id: app.util.uuid()}]
+      },
+      {
+        userId: '111', name: '今敏', avatar: 'http://image.artful.com.cn/artful/other/2017-12-22/a6509c8747e0dfb5!960xAuto',
+        list:[
+          {url: 'http://image.artful.com.cn/artful/other/2017-12-25/1a87be94254d4cd8!960xAuto', id: app.util.uuid()},
+          {url:  'http://image.artful.com.cn/artful/other/2017-12-22/a6509c8747e0dfb5!960xAuto', id: app.util.uuid()},
+          {url:  'http://image.artful.com.cn/artful/other/2017-12-22/30726fac9c4060d1!960xAuto', id: app.util.uuid()}]
+      }
+    ]
   },
   /**
    * 生命周期函数--监听页面加载
