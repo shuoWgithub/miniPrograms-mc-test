33728d00b617eeef0b426e8d64c3caf42f28d0a3
liangmengyuan <liangmengyuan@wecsh.cn>
2017-12-29 14:47:58 +0800

    follow页面

---
+++ src/pages/personal/follow/index.js
@@ -0,0 +1,10 @@
+const app = getApp()
+
+Page({
+  data: {
+    title: 'follow page',
+    title: '我的关注',
+    tabbar: {},
+    imgUrl: 'http://image.artful.com.cn/appbanner/64958109_p0_master1200.jpg'
+  }
+})
