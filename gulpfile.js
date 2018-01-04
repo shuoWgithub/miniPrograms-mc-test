e7c586d3e263664f6c911d74328fe88fb88d1fe2
RAN <wangshuomailbox@163.com>
2018-01-04 18:37:05 +0800

    ws

--- gulpfile.js
+++ gulpfile.js
@@ -3,43 +3,22 @@ const path = require('path')
 
 const gulp = require('gulp')
 const gulpLoadPlugins = require('gulp-load-plugins')
+const plumber = require('gulp-plumber');
+const notify = require("gulp-notify")
 const del = require('del')
 const runSequence = require('run-sequence')
-const inquirer = require('inquirer')
-const generatePage = require('generate-weapp-page')
 
 // load all gulp plugins
 const plugins = gulpLoadPlugins()
 const env = process.env.NODE_ENV || 'development'
 const isProduction = () => env === 'production'
 
-// utils functions
-function generateFile (options) {
-  const files = generatePage({
-    root: path.resolve(__dirname, './src/pages/'),
-    name: options.pageName,
-    less: options.styleType === 'less',
-    scss: options.styleType === 'scss',
-    css: options.styleType === 'css',
-    json: options.needConfig
-  })
-  files.forEach && files.forEach(file => plugins.util.log('[generate]', file))
-  return files
-}
-
-function generateJson (options) {
-  const filename = path.resolve(__dirname, 'src/app.json')
-  const now = fs.readFileSync(filename, 'utf8')
-  const temp = now.split('\n    // Dont remove this comment')
-  if (temp.length !== 2) {
-    return plugins.util.log('[generate]', 'Append json failed')
+function handleErrors(errorObject, callback) {
+  notify.onError(errorObject.toString().split(': ').join(':\n')).apply(this, arguments);
+  // Keep gulp from hanging on this task
+  if (typeof this.emit === 'function') {
+    this.emit('end');
   }
-  const result = `${temp[0].trim()},
-    "pages/${options.pageName}/${options.pageName}"
-    // Dont remove this comment
-  ${temp[1].trim()}
-`
-  fs.writeFileSync(filename, result)
 }
 
 /**
@@ -47,25 +26,19 @@ function generateJson (options) {
  */
 gulp.task('clean', del.bind(null, ['dist/*']))
 
-/**
- * Lint source code
- */
-// gulp.task('lint', () => {
-//   return gulp.src(['*.{js,json}', '**/*.{js,json}', '!node_modules/**', '!dist/**', '!**/bluebird.js'])
-//     .pipe(plugins.eslint())
-//     .pipe(plugins.eslint.format('node_modules/eslint-friendly-formatter'))
-//     .pipe(plugins.eslint.failAfterError())
-// })
 
 /**
  * Compile js source to distribution directory
  */
 gulp.task('compile:js', () => {
   return gulp.src(['src/**/*.js'])
-    .pipe(plugins.sourcemaps.init())
-    .pipe(plugins.babel())
+    .pipe(plumber(handleErrors))
+    .pipe(plugins.newer('dist'))
+    .pipe(plugins.logger({ showChange: true }))
+    .pipe(plugins.babel({
+      presets: ['es2015']
+    }))
     .pipe(plugins.if(isProduction, plugins.uglify()))
-    .pipe(plugins.sourcemaps.write('.'))
     .pipe(gulp.dest('dist'))
 })
 
@@ -74,19 +47,18 @@ gulp.task('compile:js', () => {
  */
 gulp.task('compile:xml', () => {
   return gulp.src(['src/**/*.xml'])
-    .pipe(plugins.sourcemaps.init())
+    .pipe(plumber(handleErrors))
+    .pipe(plugins.rename({ extname: '.wxml' }))
+    .pipe(plugins.newer('dist'))
+    .pipe(plugins.logger({ showChange: true }))
     .pipe(plugins.if(isProduction, plugins.htmlmin({
       collapseWhitespace: true,
-      // collapseBooleanAttributes: true,
-      // removeAttributeQuotes: true,
       keepClosingSlash: true, // xml
       removeComments: true,
       removeEmptyAttributes: true,
       removeScriptTypeAttributes: true,
       removeStyleLinkTypeAttributes: true
     })))
-    .pipe(plugins.rename({ extname: '.wxml' }))
-    .pipe(plugins.sourcemaps.write('.'))
     .pipe(gulp.dest('dist'))
 })
 
@@ -95,11 +67,12 @@ gulp.task('compile:xml', () => {
  */
 gulp.task('compile:less', () => {
   return gulp.src(['src/**/*.less'])
-    .pipe(plugins.sourcemaps.init())
+    .pipe(plumber(handleErrors))
     .pipe(plugins.less())
-    .pipe(plugins.if(isProduction, plugins.cssnano({ compatibility: '*' })))
     .pipe(plugins.rename({ extname: '.wxss' }))
-    .pipe(plugins.sourcemaps.write('.'))
+    .pipe(plugins.newer('dist'))
+    .pipe(plugins.logger({ showChange: true }))
+    .pipe(plugins.if(isProduction, plugins.cssnano({ compatibility: '*' })))
     .pipe(gulp.dest('dist'))
 })
 
@@ -108,9 +81,10 @@ gulp.task('compile:less', () => {
  */
 gulp.task('compile:json', () => {
   return gulp.src(['src/**/*.json'])
-    .pipe(plugins.sourcemaps.init())
+    .pipe(plumber(handleErrors))
+    .pipe(plugins.newer('dist'))
+    .pipe(plugins.logger({ showChange: true }))
     .pipe(plugins.jsonminify())
-    .pipe(plugins.sourcemaps.write('.'))
     .pipe(gulp.dest('dist'))
 })
 
@@ -119,6 +93,9 @@ gulp.task('compile:json', () => {
  */
 gulp.task('compile:img', () => {
   return gulp.src(['src/**/*.{jpg,jpeg,png,gif}'])
+    .pipe(plumber(handleErrors))
+    .pipe(plugins.newer('dist'))
+    .pipe(plugins.logger({ showChange: true }))
     .pipe(plugins.imagemin())
     .pipe(gulp.dest('dist'))
 })
@@ -148,12 +125,11 @@ gulp.task('extras', [], () => {
     '!src/**/*.json',
     '!src/**/*.{jpe?g,png,gif}'
   ])
-  .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('dist'))
 })
 
 /**
  * Build
- * add ['lint'],  befor 'bulid'
  */
 gulp.task('build', next => runSequence(['compile', 'extras'], next))
 
@@ -166,41 +142,6 @@ gulp.task('watch', ['build'], () => {
   gulp.watch('src/**/*.less', ['compile:less'])
   gulp.watch('src/**/*.json', ['compile:json'])
   gulp.watch('src/**/*.{jpe?g,png,gif}', ['compile:img'])
-})
-
-/**
- * Generate new page
- */
-gulp.task('generate', next => {
-  inquirer.prompt([
-    {
-      type: 'input',
-      name: 'pageName',
-      message: 'Input the page name',
-      default: 'index'
-    },
-    {
-      type: 'confirm',
-      name: 'needConfig',
-      message: 'Do you need a configuration file',
-      default: false
-    },
-    {
-      type: 'list',
-      name: 'styleType',
-      message: 'Select a style framework',
-      // choices: ['less', 'scss', 'css'],
-      choices: ['less'],
-      default: 'less'
-    }
-  ])
-  .then(options => {
-    const res = generateFile(options)
-    if (res) generateJson(options)
-  })
-  .catch(err => {
-    throw new plugins.util.PluginError('generate', err)
-  })
 })
 
 /**
